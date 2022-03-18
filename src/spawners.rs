fe6fa2131663c46ab69cb79ca839b61cb6644d47
Alexis Rinaldoni <alexis.rinaldoni@be-terna.com>
2022-03-18 23:31:13 +0100

    Chapter 8

--- src/spawners.rs
+++ src/spawners.rs
@@ -4,6 +4,10 @@ pub fn spawn_player(ecs: &mut World, pos: Point) {
     ecs.push((
         Player,
         pos,
+        Health {
+            current: 10,
+            max: 10,
+        },
         Render {
             color: ColorPair::new(WHITE, BLACK),
             glyph: to_cp437('@'),
@@ -12,18 +16,30 @@ pub fn spawn_player(ecs: &mut World, pos: Point) {
 }
 
 pub fn spawn_monster(ecs: &mut World, rng: &mut RandomNumberGenerator, pos: Point) {
+    let (hp, name, glyph) = match rng.roll_dice(1, 10) {
+        1..=8 => goblin(),
+        _ => orc(),
+    };
     ecs.push((
         Enemy,
         pos,
+        MovingRandomly,
+        Name(name),
+        Health {
+            max: hp,
+            current: hp,
+        },
         Render {
             color: ColorPair::new(GREEN, RED),
-            glyph: match rng.range(0, 4) {
-                0 => to_cp437('E'),
-                1 => to_cp437('O'),
-                2 => to_cp437('o'),
-                _ => to_cp437('g'),
-            },
+            glyph,
         },
-        MovingRandomly,
     ));
+}
+
+pub fn goblin() -> (i32, String, FontCharType) {
+    (1, String::from("Goblin"), to_cp437('g'))
+}
+
+pub fn orc() -> (i32, String, FontCharType) {
+    (2, String::from("Orc"), to_cp437('o'))
 }
